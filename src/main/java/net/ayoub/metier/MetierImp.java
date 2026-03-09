340982916b761ea2e9fbfc5fd495b8c4453a7638
7ayoub15 <SAMY82696@GMAIL.COM>
2026-03-09 10:29:05 +0000

    fixed the problem of two Components using Qualified annotation

--- src/main/java/net/ayoub/metier/MetierImp.java
+++ src/main/java/net/ayoub/metier/MetierImp.java
@@ -2,25 +2,28 @@ package net.ayoub.metier;
 
 import net.ayoub.dao.IDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component("metier")
 public class MetierImp implements IMetier{
     @Autowired
+    @Qualifier("d")
     private IDao dao ; // couplage faible (depend des interfaces mais pas des classes)
 //injection des  dependances par le constructeur avec parametre
 
-    public MetierImp(IDao dao) {
+    public MetierImp(@Qualifier("d") IDao dao) {
+
         this.dao = dao;
     }
 // injection des dependances en utilisant le setters
 
-    public MetierImp() {
-    }
-
-    public void setDao(IDao dao) {
-        this.dao = dao;
-    }
+//    public MetierImp() {
+//    }
+//
+//    public void setDao(IDao dao) {
+//        this.dao = dao;
+//    }
 
     @Override
     public double calcul() {
